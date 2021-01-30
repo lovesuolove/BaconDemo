da419bb2f082be41937061034fe791bcb87b61a4
Suo <lovesuolove@gmail.com>
2021-01-30 09:26:33 +0800

    修改基础配置信息

--- Example/Pods/Target Support Files/BaconDemo/BaconDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BaconDemo : NSObject
-@end
-@implementation PodsDummy_BaconDemo
-@end
--- Example/Pods/Target Support Files/Pods-BaconDemo_Tests/Pods-BaconDemo_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BaconDemo_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_BaconDemo_Tests
-@end
